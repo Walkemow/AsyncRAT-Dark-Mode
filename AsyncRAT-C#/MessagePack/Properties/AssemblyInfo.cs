05ea306442e9f5fd72b67122d0ceb9a02ca8eeb3
VisionsXZ <64665287+VisionsXZ@users.noreply.github.com>
2025-11-20 23:02:07 +0400

    AssemblyInfo.cs

---
+++ AsyncRAT-C#/MessagePack/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("8.26.472.5235")]
+[assembly: AssemblyFileVersion("8.26.472.5235")]
